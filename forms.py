b235cb3d0476db87a052382b534f06a8bb7565b3
Silas Leonardo <silas.leonardo15@gmail.com>
2020-03-07 13:30:11 -0300

    Resolucao de bugs e outras atualizacoes

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField,DateField
+from wtforms import StringField, TextAreaField, DateField
 from wtforms.validators import DataRequired
 
 
 class CrimeForm(FlaskForm):
-    descricao = StringField('Descricao', validators=[DataRequired()])
+    descricao = StringField('descricao', validators=[DataRequired()])
     data = DateField('data', validators=[DataRequired()])
     crime = StringField('crime', validators=[DataRequired()])
